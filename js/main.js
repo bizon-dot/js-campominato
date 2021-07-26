812cf807f27e0a0deeae71935206495a697a7f91
robertoAureli <r.aureli@protonmail.com>
2021-07-26 12:52:51 +0200

    Funzione Gioco: L'utente gioca inserendo 84 volte un input numerico che non si deve ripetere. Il gioco termina se trova la bomba  o se finisco i numeri consentiti

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 /*
 
-Il computer deve generare 16 numeri casuali (le nostre bombe) tra 1 e 100.I numeri non possono essere duplicati.
-In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.L’utente non può inserire più volte lo stesso numero.
-Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+//  1. Il computer deve generare 16 numeri casuali (le nostre bombe) tra 1 e 100.I numeri non possono essere duplicati.
+    
+    2. In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.L’utente non può inserire più volte lo stesso numero. Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+    
+    3. La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+    
+    4.Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
 
 */
@@ -30,3 +32,32 @@ function arrayBombs(){
     
 }
 
+/*  
+    ========================================================================================================
+        2.                              L'utente gioca inserendo 84 volte un input numerico 
+                                        che non si deve ripetere. Il gioco termina se trova la bomba 
+                                        o se finisco i numeri consentiti
+    ========================================================================================================
+    
+*/
+
+function game(){
+    let playerNumbers = [];
+    let bombs = arrayBombs();
+    console.log(bombs);
+    for (let index = 0; index < 84; index++) {
+        let playerNum = prompt("Inserisci un numero da 1 a 100");
+        if (!(playerNumbers.includes(playerNum))) {
+            if ((bombs.includes(parseInt(playerNum)))){
+                console.log("Bomba!");
+            }
+            playerNumbers.push(playerNum);
+            
+        }
+        console.log(playerNumbers);
+    }
+}
+
+
+
+game();
