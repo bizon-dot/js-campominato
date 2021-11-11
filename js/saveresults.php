95ab9e097bee6fff65d17354ec44bc13c5c82e84
robertoAureli <r.aureli@protonmail.com>
2021-11-11 10:33:10 +0100

    up screen

--- js/saveresults.php
+++ js/saveresults.php
@@ -1,18 +1,13 @@
 <?php
-class User
-{
-    public $nick;
-    public $points;
-}
+
+
 // get the q parameter from URL
 $nick = $_REQUEST["nick"];
 // get the q parameter from URL
 $points = $_REQUEST["points"];
 if ($nick !== "") {
-    $user = new User();
-    $user->nick = $nick;
-    $user->points =$points;
-    $fp = fopen('points.json', 'w+');
-    fwrite($fp, json_encode($user));
+    $file = fopen("results.txt", "a");
+    // save nick and points as json in results.txt
+    fwrite($file, json_encode(array("nick" => $nick, "points" => $points)) . "\n");
     fclose($fp);
 }
